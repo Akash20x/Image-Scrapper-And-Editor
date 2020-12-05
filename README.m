b19ab5b9556baf82745baac0547e03ce538729fa
Akash20x <46225357+Akash20x@users.noreply.github.com>
2020-12-06 02:33:17 +0530

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,34 @@
+# Image-Scrapper-And-Editor
+Tkinter Image Creater
+
+# Scrapping + Editing + Merging Images
+This python app is made using tkinter GUI library
+Check Out and Collab!
+
+# Following things this app can do:
+1.Scrapped main image from google images on the image window
+2.We can switch between various images of 1 query on the main window
+3.Add many images according to our size on the main image(Using Megre Button)
+4.Quick Collage Making
+5.Add text on image
+6.Scrap text meaning and also add it on image
+7.Resize Both main image and merged image
+8.Draw on the image
+
+# Remaining Features to add: 
+1.To add image filters 
+2.Crop and Rotate
+3.To draw different shapes on image canvas
+4.Brush type and size
+5.Fixing saving image problem
+6.Undo
+7.Text font and size
+
+These features will be add in future.
+You can collab by adding pull request to add functionality of remaining features or any other bug fixing!
+
+
+![app](https://user-images.githubusercontent.com/46225357/101262596-067d5100-3766-11eb-98db-2608bcea722a.png)
+
+![test](https://user-images.githubusercontent.com/46225357/101262986-201f9800-3768-11eb-9d1b-61132ede1678.png)
+
--- README.md
+++
@@ -1,8 +0,0 @@
-# Image-Scrapper-And-Editor
-Tkinter Image Creater
-
-# Scrapping + Editing + Merging Images
-![app](https://user-images.githubusercontent.com/46225357/101262596-067d5100-3766-11eb-98db-2608bcea722a.png)
-
-![test](https://user-images.githubusercontent.com/46225357/101262986-201f9800-3768-11eb-9d1b-61132ede1678.png)
-
